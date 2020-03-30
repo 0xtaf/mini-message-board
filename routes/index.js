05f66b76e6b9b2683e3f910fbce15ee0a8c05a6c
Tayfun Sur <tayfunsur@gmail.com>
2020-03-30 15:14:59 +0300

    ready to be pushed to heroku

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+
+const messages = [ 
+  { text: 'Hi there!', user: 'Amando', added: new Date() },
+  { text: 'Hello World!', user: 'Charles', added: new Date() } 
+];
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index1', { title: 'Mini Messageboard', messagesVar: messages });
+});
+
+router.get('/new', function(req, res, next){
+  res.render('form', {title: 'Send your message'} )
+})
+
+router.post('/new', function(req, res, next){
+  messages.push({text: req.body.message, user: req.body.name, added: new Date()}); 
+  res.redirect('/');
+}) 
+
+module.exports = router;
